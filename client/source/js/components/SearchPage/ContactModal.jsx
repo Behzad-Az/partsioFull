8894233ce83845bb6476210aca9ba93447b0e2a3
Behzad-Az <behy1368@gmail.com>
2017-11-15 19:23:05 -0800

    fixed defaultValue bug for subject line in contactModal.jsx

--- client/source/js/components/SearchPage/ContactModal.jsx
+++ client/source/js/components/SearchPage/ContactModal.jsx
@@ -16,16 +16,110 @@ export default class ContactModal extends Component {
   constructor() {
     super();
     this._closeModal = this._closeModal.bind(this);
+    this._renderBody = this._renderBody.bind(this);
   }
 
   _closeModal() {
     this.props.dispatch(spToggleModal({ id: null }));
   }
 
+  _renderBody() {
+    const { item } = this.props.modalParams;
+    if (item) {
+      const subject = item ? `Re: ${item._source.title} for Sale` : 'Re: Item for Sale';
+      return (
+        <section className='modal-card-body'>
+          <div className='field is-horizontal'>
+            <div className='field-label is-normal'>
+              <label className='label'>From</label>
+            </div>
+            <div className='field-body'>
+              <div className='field'>
+                <p className='control is-expanded has-icons-left'>
+                  <input className='input' type='text' placeholder='Name' />
+                  <span className='icon is-small is-left'>
+                    <i className='fa fa-user' />
+                  </span>
+                </p>
+              </div>
+              <div className='field'>
+                <p className='control is-expanded has-icons-left has-icons-right'>
+                  <input className='input' type='email' placeholder='Email' />
+                  <span className='icon is-small is-left'>
+                    <i className='fa fa-envelope' />
+                  </span>
+                  <span className='icon is-small is-right'>
+                    <i className='fa fa-check' />
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='field is-horizontal'>
+            <div className='field-label'></div>
+            <div className='field-body'>
+              <div className='field is-expanded'>
+                <div className='field has-addons'>
+                  <p className='control'>
+                    <a className='button is-static'>
+                      +1
+                    </a>
+                  </p>
+                  <p className='control is-expanded'>
+                    <input className='input' type='tel' placeholder='Phone number (optional)' />
+                  </p>
+                </div>
+                <p className='help'>Include area code</p>
+              </div>
+            </div>
+          </div>
+          <div className='field is-horizontal'>
+            <div className='field-label is-normal'>
+              <label className='label'>Subject</label>
+            </div>
+            <div className='field-body'>
+              <div className='field'>
+                <div className='control'>
+                  <input
+                    className='input'
+                    type='text'
+                    placeholder='e.g. Bidding on your item for sale'
+                    defaultValue={subject}
+                  />
+                </div>
+                <p className='help is-danger'>
+                  This field is required
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='field is-horizontal'>
+            <div className='field-label is-normal'>
+              <label className='label'>Message</label>
+            </div>
+            <div className='field-body'>
+              <div className='field'>
+                <div className='control'>
+                  <textarea className='textarea' placeholder='Type your message here'></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    } else {
+      return (
+        <section className='modal-card-body has-text-centered'>
+          <i className='fa fa-cog fa-spin fa-3x fa-fw' />
+          <span className='sr-only'>Loading...</span>
+        </section>
+      );
+    }
+  }
+
   render() {
-    const { id } = this.props.modalParams;
     return (
-      <div className={id === 'contactModal' ? 'modal is-active' : 'modal'}>
+      <div className={this.props.modalParams.id === 'contactModal' ? 'modal is-active' : 'modal'}>
         <div className='modal-background' onClick={this._closeModal}></div>
         <div className='modal-card'>
           <header className='modal-card-head'>
@@ -34,9 +128,7 @@ export default class ContactModal extends Component {
             </p>
             <button className='delete' onClick={this._closeModal}></button>
           </header>
-          <section className='modal-card-body'>
-            <p>TBD</p>
-          </section>
+          { this._renderBody() }
           <footer className='modal-card-foot'>
             <button className='button is-success'>Send</button>
             <button className='button' onClick={this._closeModal}>Cancel</button>
