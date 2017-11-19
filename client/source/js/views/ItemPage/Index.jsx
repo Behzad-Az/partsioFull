d8f74ebc75a5e74aef0558dbc12a39ef104f2a6e
Behzad-Az <behy1368@gmail.com>
2017-11-19 13:19:04 -0800

    broke down the item page components into individual ones

--- client/source/js/views/ItemPage/Index.jsx
+++ client/source/js/views/ItemPage/Index.jsx
@@ -28,6 +28,11 @@ export default class ItemPage extends Component {
     this._decodeCompanyRating = this._decodeCompanyRating.bind(this);
     this._renderDocsTable = this._renderDocsTable.bind(this);
     this._renderGallery = this._renderGallery.bind(this);
+    this._renderIntroTile = this._renderIntroTile.bind(this);
+    this._renderDocsTile = this._renderDocsTile.bind(this);
+    this._renderPhotosTile = this._renderPhotosTile.bind(this);
+    this._renderCommentsTile = this._renderCommentsTile.bind(this);
+    this._renderContactTile = this._renderContactTile.bind(this);
     this._renderCompAfterData = this._renderCompAfterData.bind(this);
   }
 
@@ -97,6 +102,144 @@ export default class ItemPage extends Component {
     }
   }
 
+  _renderIntroTile() {
+    const { title } = this.props.item._source;
+    return (
+      <article className='tile is-child notification is-purple'>
+        <p className='title has-text-white'>{title}</p>
+        <p className='subtitle has-text-white'>{ this._decodeCompanyRating() }</p>
+        <div className='field'>
+          <div className='control'>
+            <button className='button is-fullwidth'>Buy Now!</button>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
+  _renderDocsTile() {
+    return (
+      <article className='tile is-child notification is-purple'>
+        <p className='title has-text-white'>
+          <i className='fa fa-paperclip' /> Docs
+        </p>
+        <table className='table is-fullwidth is-hoverable'>
+          <thead>
+            <tr>
+              <th style={{ width: '70%' }}>Title</th>
+              <th style={{ width: '30%' }} className='has-text-centered'><i className='fa fa-cloud-download' /></th>
+            </tr>
+          </thead>
+          <tbody>
+            { this._renderDocsTable() }
+          </tbody>
+        </table>
+      </article>
+    );
+  }
+
+  _renderPhotosTile() {
+    return (
+      <article className='tile is-child notification is-purple'>
+        <p className='title has-text-white'>
+          <i className='fa fa-camera' /> Photos
+        </p>
+        <p className='subtitle has-text-white'>With an image</p>
+        { this._renderGallery() }
+      </article>
+    );
+  }
+
+  _renderCommentsTile() {
+    return (
+      <article className='tile is-child notification is-purple'>
+        <p className='title has-text-white'>
+          <i className='fa fa-comment-o' /> Comments
+        </p>
+        <p className='content has-text-white'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+        </p>
+      </article>
+    );
+  }
+
+  _renderContactTile() {
+    const { item } = this.props;
+    const subject = item ? `Re: ${item._source.title} for Sale` : 'Re: Item for Sale';
+    return (
+      <article className='tile is-child notification is-purple'>
+        <div className='content'>
+          <p className='title has-text-white'>
+            <i className='fa fa-reply' /> Contact / Buy
+          </p>
+          <div className='content'>
+            <div className='field'>
+              <label className='label has-text-white'>From</label>
+              <div className='control has-icons-left'>
+                <input className='input' type='text' placeholder='Name' />
+                <span className='icon is-small is-left'>
+                  <i className='fa fa-user' />
+                </span>
+              </div>
+            </div>
+            <div className='field'>
+              <div className='control has-icons-left has-icons-right'>
+                <input className='input' type='email' placeholder='Email' />
+                <span className='icon is-small is-left'>
+                  <i className='fa fa-envelope' />
+                </span>
+                <span className='icon is-small is-right'>
+                  <i className='fa fa-check' />
+                </span>
+              </div>
+            </div>
+            <div className='field has-addons'>
+              <div className='control'>
+                <a className='button is-static'>
+                  +1
+                </a>
+              </div>
+              <div className='control is-expanded'>
+                <input className='input' type='tel' placeholder='Phone number (optional)' />
+              </div>
+            </div>
+            <div className='field'>
+              <label className='label has-text-white'>Subject</label>
+              <div className='control'>
+                <input
+                  className='input'
+                  type='text'
+                  placeholder='e.g. Bidding on your item for sale'
+                  defaultValue={subject}
+                />
+              </div>
+            </div>
+            <div className='field'>
+              <label className='label has-text-white'>Message</label>
+              <div className='control'>
+                <textarea className='textarea' placeholder='Textarea' />
+              </div>
+            </div>
+            <div className='field is-grouped'>
+              <div className='control'>
+                <button className='button'>Submit</button>
+              </div>
+              <div className='control'>
+                <button className='button is-text has-text-white'>Cancel</button>
+              </div>
+            </div>
+            <hr />
+            <div className='field'>
+              <div className='control'>
+                <button className='button is-fullwidth'>Buy Now!</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
   _renderCompAfterData() {
     const { asyncLoading, asyncError, dataLoaded, item, dispatch } = this.props;
     if (asyncLoading) {
@@ -107,133 +250,24 @@ export default class ItemPage extends Component {
         </p>
       );
     } else if (dataLoaded && !asyncError) {
-      const { title, search_text, photos, id } = item._source;
-      const numPhotos = photos.length;
-      const subject = item ? `Re: ${item._source.title} for Sale` : 'Re: Item for Sale';
       return (
         <div className='tile is-ancestor'>
           <div className='tile is-vertical is-8'>
             <div className='tile'>
               <div className='tile is-parent is-vertical'>
-                <article className='tile is-child notification is-purple'>
-                  <p className='title has-text-white'>{title}</p>
-                  <p className='subtitle has-text-white'>{ this._decodeCompanyRating() }</p>
-                  <div className='field'>
-                    <div className='control'>
-                      <button className='button is-fullwidth'>Buy Now!</button>
-                    </div>
-                  </div>
-                </article>
-                <article className='tile is-child notification is-purple'>
-                  <p className='title has-text-white'>
-                    <i className='fa fa-paperclip' /> Docs
-                  </p>
-                  <table className='table is-fullwidth is-hoverable'>
-                    <thead>
-                      <tr>
-                        <th style={{ width: '70%' }}>Title</th>
-                        <th style={{ width: '30%' }} className='has-text-centered'><i className='fa fa-cloud-download' /></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      { this._renderDocsTable() }
-                    </tbody>
-                  </table>
-                </article>
+                { this._renderIntroTile() }
+                { this._renderDocsTile() }
               </div>
               <div className='tile is-parent'>
-                <article className='tile is-child notification is-purple'>
-                  <p className='title has-text-white'>
-                    <i className='fa fa-camera' /> Photos
-                  </p>
-                  <p className='subtitle has-text-white'>With an image</p>
-                  { this._renderGallery() }
-
-                </article>
+                { this._renderPhotosTile() }
               </div>
             </div>
             <div className='tile is-parent'>
-              <article className='tile is-child notification is-purple'>
-                <p className='title has-text-white'>
-                  <i className='fa fa-comment-o' /> Comments
-                </p>
-                <p className='content has-text-white'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                </p>
-              </article>
+              { this._renderCommentsTile() }
             </div>
           </div>
           <div className='tile is-parent'>
-            <article className='tile is-child notification is-purple'>
-              <div className='content'>
-                <p className='title has-text-white'>
-                  <i className='fa fa-reply' /> Contact / Buy
-                </p>
-                <div className='content'>
-                  <div className='field'>
-                    <label className='label has-text-white'>From</label>
-                    <div className='control has-icons-left'>
-                      <input className='input' type='text' placeholder='Name' />
-                      <span className='icon is-small is-left'>
-                        <i className='fa fa-user' />
-                      </span>
-                    </div>
-                  </div>
-                  <div className='field'>
-                    <div className='control has-icons-left has-icons-right'>
-                      <input className='input' type='email' placeholder='Email' />
-                      <span className='icon is-small is-left'>
-                        <i className='fa fa-envelope' />
-                      </span>
-                      <span className='icon is-small is-right'>
-                        <i className='fa fa-check' />
-                      </span>
-                    </div>
-                  </div>
-                  <div className='field has-addons'>
-                    <div className='control'>
-                      <a className='button is-static'>
-                        +1
-                      </a>
-                    </div>
-                    <div className='control is-expanded'>
-                      <input className='input' type='tel' placeholder='Phone number (optional)' />
-                    </div>
-                  </div>
-                  <div className='field'>
-                    <label className='label has-text-white'>Subject</label>
-                    <div className='control'>
-                      <input
-                        className='input'
-                        type='text'
-                        placeholder='e.g. Bidding on your item for sale'
-                        defaultValue={subject}
-                      />
-                    </div>
-                  </div>
-                  <div className='field'>
-                    <label className='label has-text-white'>Message</label>
-                    <div className='control'>
-                      <textarea className='textarea' placeholder='Textarea' />
-                    </div>
-                  </div>
-                  <div className='field is-grouped'>
-                    <div className='control'>
-                      <button className='button'>Submit</button>
-                    </div>
-                    <div className='control'>
-                      <button className='button is-text has-text-white'>Cancel</button>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className='field'>
-                    <div className='control'>
-                      <button className='button is-fullwidth'>Buy Now!</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
+            { this._renderContactTile() }
           </div>
         </div>
       );
